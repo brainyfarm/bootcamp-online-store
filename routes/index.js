9dab8309e6bb59fa9f7e742fdcf1113370ac632e
Olawale Akinseye <brainyfarm@gmail.com>
2016-10-26 02:21:22 +0100

    Added unique url generator and manage route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,31 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
 
+/* A unique url generator */
+var uniqueUrl = (function(min, max) {
+	var randomChar = function(){
+		return String.fromCharCode(Math.floor(Math.random() * (max - min + 1) + min));
+	};
+	
+  return [
+  	randomChar(),
+  	randomChar().toLowerCase(),
+  	randomChar(),
+  	randomChar().toLowerCase()].join("");
+})(65, 77);
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to Store!' });
 });
 
+
+router.get('/manage/:store_id', function(req, res){
+
+      res.send("Manage Store");
+})
 router.get('/dashboard', function(req, res, next){
   /* If no user is signed in */
   if(!global.currentUser)
@@ -17,6 +37,8 @@ router.get('/dashboard', function(req, res, next){
   var usersRef = ref.child("users/" + global.currentUserID  +'/' );
   var userStoreRef = ref.child("stores/" + global.currentUserID  +'/' );
 
+  var storeOwnersRef = ref.child("owners/")
+
   usersRef.on('value', function(snapshot){
     console.log(snapshot.val());
     var userName = snapshot.val().firstname;
@@ -25,20 +47,76 @@ router.get('/dashboard', function(req, res, next){
       userStoreRef.on('value', function(snapshot){
           var userStoreObject = snapshot.val();
           var userLink;
-          if(!userStoreObject){
+          if(!userName){
             userLink = null;
           }
           else{
-            userLink = userStoreObject.link;
+
+            storeOwnersRef.on('value', function(snapshot){
+              userLink = snapshot.val().currentUserID || null;
+              console.log(userLink);
+              
+            })
+
           }
-          console.log(userStoreObject);
-          res.render('dashboard', {title: 'Dashboard', currentUser : global.currentUser, currentUserName : userName, currentUserLink: userStoreObject });
+          //console.log(userStoreObject);
+
+
+          res.render('dashboard', {title: 'Dashboard', currentUser : global.currentUser, 
+          currentUserName : userName, currentUserLink: userLink,
+          freeUrl:uniqueUrl 
+          });
 
       })
      
 
   })
     
+})
+
+
+router.post('/dashboard', function(req, res, next){
+    var userID = global.currentUserID;
+    var storename = req.body.storename;
+    var storelink = uniqueUrl;
+
+    console.log(req.body);
+
+    console.log(currentUserID);
+    console.log(storename, "storname");
+    console.log(storelink, "storelink");
+
+    var db = firebase.database();
+    var ref = db.ref('/');
+    var userStoreRef = ref.child("stores/" + storelink  );
+    var ownersRef = ref.child("owners");
+
+
+  //console.log(userStoreRef);
+    
+    userStoreRef.set({
+        storeowner : global.currentUserID,
+        storename : storename,
+        storelink: storelink,
+        products: []
+    })
+    // .then(function(response){
+    // //console.log(response);
+        
+    // })
+    // .catch(function(error){
+    //   console.log(error.message);
+    // })
+
+    ownersRef.update({
+            [currentUserID] : storelink
+    })
+
+    res.redirect('/manage/' + storelink);
+
+
+  
+
 })
 
 router.get('/signup', function(req, res, next) {
