e90accf84305f9085527538ffa5637b2ec032a67
Olawale Akinseye <brainyfarm@gmail.com>
2016-10-28 03:03:42 +0100

    Added login checker middleware

--- routes/login-middleware.js
+++ routes/login-middleware.js
@@ -1,4 +1,4 @@
-const firebase = require('firebase');
+const firebase = require('../helpers/firebase.js');
 
 module.exports = function loginMiddleware(req, res, next) {
   const user = firebase.auth().currentUser;
@@ -6,7 +6,19 @@ module.exports = function loginMiddleware(req, res, next) {
   if (user) {
     const { uid, email } = user;
 
-    req.user = { id: uid, email };
+    let name, phone, store;
+    
+    // A hackish session.
+    // Todo: Find a better way. A proper session storage maybe?
+    // This is set in auth.js UpdateUserInfo
+    // Yes, I know globals are bad. :(
+    if (global.userInfo) {
+      name = global.userInfo.firstname;
+      store = global.userInfo.store;
+      phone = global.userInfo.phone;
+    }
+
+    req.user = { id: uid, email, name, phone, store };
     next();
   } else {
     res.redirect('/login');
